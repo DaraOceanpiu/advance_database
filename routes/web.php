e2b89d50224e8d440a2cb6fd64fcec973dafd5ee
Kosal Vathanak <vathanak110@gmail.com>
2024-05-12 14:54:49 +0700

    input ui inside view. thank to sokleng

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
+});
+Route::get('/wallets', function () {
+    return view('wallets.index');
+});
+Route::get('/pending', function () {
+    return view('pending.index');
 });
